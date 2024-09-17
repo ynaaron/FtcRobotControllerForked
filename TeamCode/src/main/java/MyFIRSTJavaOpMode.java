f8641365e61c8129b9470d0739bc170dc64e9a68
Naing Lin Htet <aaronyan002006@gmail.com>
2024-09-17 03:59:18 -0400

    First change

---
+++ TeamCode/src/main/java/MyFIRSTJavaOpMode.java
@@ -0,0 +1,14 @@
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class MyFIRSTJavaOpMode extends LinearOpMode{
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        
+    }
+}
